b0d12aae3f78b1694122a4ce9a38bf77c7486d97
Wesenheit <matio@opoczta.pl>
2021-05-13 00:05:22 +0200

    added multiprocessing

--- MultiProcessing/process.py
+++ MultiProcessing/process.py
@@ -1,2 +1,35 @@
 import os
-import multiprocessing
+import multiprocessing as mp
+from ctypes import *
+
+class Manager:
+
+    def __init__(self,tab,fun):
+        self.name=self
+        self.tab=tab
+        self.fun=fun
+        self.n=n=mp.cpu_count()
+
+    def __call__(self):
+
+    	def pom(tab,v,fun):
+    		wyn = 0
+    		for a in tab:
+    			wyn += fun(a)
+    		with v.get_lock():
+        		v.value += wyn
+
+
+    	v=mp.Value(c_longlong,0)
+    	processes=[]
+    	for c in range(0,n):
+    		temp=[self.tab[i] for i in range(c,len(self.tab),self.n)]
+    		p=mp.Process(target = pom, args = (temp,v,self.fun))
+    		processes.append(p)
+
+		for p in processes:
+    		p.start()
+
+		for p in processes:
+    		p.join()
+    	return v.value
