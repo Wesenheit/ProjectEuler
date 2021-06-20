7714052f299fb5a7322d19e84e59016c3100f567
Wesenheit <matio@opoczta.pl>
2021-06-20 14:42:34 +0200

    time measurments

--- MultiProcessing/process.py
+++ MultiProcessing/process.py
@@ -1,5 +1,7 @@
 import os
 import multiprocessing as mp
+import resource
+import time
 from ctypes import *
 __all__ = ['Manager']
 
@@ -10,9 +12,25 @@ class Manager:
         self.name = self
         self.tab = tab
         self.fun = fun
-        self.n = mp.cpu_count()
+        self.n = mp.cpu_count()//2
+    
+    
+    def Mes(self,q):
+        start=time.time()
+        temp=True
+        max_usage = 0
+        while temp:
+            if not q.empty():
+                temp=False
+            max_usage = max(
+                    max_usage,
+                    resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+                )
+            time.sleep(0.1)
+        end=time.time()
+        print("Elapsed time: {}, memory usage: {} kb".format(end-start,max_usage))
 
-    def __call__(self):
+    def __call__(self,measure=False):
 
         def pom(tab, v, fun):
             wyn = 0
@@ -27,10 +45,18 @@ class Manager:
             temp = [self.tab[i] for i in range(c, len(self.tab), self.n)]
             p = mp.Process(target=pom, args=(temp, v, self.fun))
             processes.append(p)
-
+        if measure:
+            q=mp.Queue()
+            monitor=mp.Process(target=self.Mes ,args=(q,))
+            monitor.start()
+        
         for p in processes:
             p.start()
 
         for p in processes:
             p.join()
+
+        if measure:
+            q.put(False)
+            monitor.join()
         return v.value
